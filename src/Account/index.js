4d9a4ceb63913b2cbd261f0a6ff805b4b5b83588
katiacreator <katia.creator@gmail.com>
2021-08-03 19:07:48 -0400

    write functionality that handles edge case for not a number

--- src/Account/index.js
+++ src/Account/index.js
@@ -1,52 +1,55 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from "react";
 
 class Account extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       account: 0,
       balance: 0,
       input: 0,
-    }
+    };
   }
-
-  //write deposit function
 
   deposit = () => {
-    this.setState({
-      balance: this.state.amount + this.state.balance
-    })
-  }
-  //write withdrawal function
-  withdrawal = () => {
-    this.setState({
-      balance: this.state.balance - this.state.amount
-    })
-  }
-  
-  handleChange = (e)=>{
-    if(!isNaN(e.target.value)){
+    if (!isNaN(this.state.input)) {
       this.setState({
-        amount: parseInt(e.target.value)
-      })
+        balance: this.state.amount + this.state.balance,
+      });
     }
-  }
+  };
+
+  withdraw = () => {
+    if (!isNaN(this.state.input)) {
+      this.setState({
+        balance: this.state.balance - this.state.amount,
+      });
+    }
+  };
+
+  handleChange = (e) => {
+    if (!isNaN(e.target.value)) {
+      this.setState({
+        amount: parseInt(e.target.value),
+      });
+    }
+  };
 
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className={this.state.balance === 0 ?
-          "zero" : "balance"}>${this.state.balance}</div>
-        <input type="text" onChange={this.handleChange} 
-        placeholder="enter an amount" />
-        <input type="button" onClick={this.deposit} 
-        value="Deposit" />
-        <input type="button" onClick={this.withdrawal} value="Withdraw" />
+        <div className={this.state.balance === 0 ? "zero" : "balance"}>
+          ${this.state.balance}
+        </div>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          placeholder="enter an amount"
+        />
+        <input type="button" onClick={this.deposit} value="Deposit" />
+        <input type="button" onClick={this.withdraw} value="Withdraw" />
       </div>
-    )
+    );
   }
 }
 
