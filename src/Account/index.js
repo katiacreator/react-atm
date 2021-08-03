35cd794d29769a416e5f94fa40c7c2150c310a38
katiacreator <katia.creator@gmail.com>
2021-08-03 18:56:03 -0400

    write deposit function

--- src/Account/index.js
+++ src/Account/index.js
@@ -3,13 +3,41 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(){
+    super()
+    this.state = {
+      account: 0,
+      balance: 0,
+      input: 0,
+    }
+  }
+
+  //write deposit function
+
+  deposit = () => {
+    this.setState({
+      balance: this.state.amount + this.state.balance
+    })
+  }
+  //write withdrawal function
+
+  handleChange = (e)=>{
+    if(!isNaN(e.target.value)){
+      this.setState({
+        amount: parseInt(e.target.value)
+      })
+    }
+  }
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
+        <div className="balance">${this.state.balance}</div>
+        <input type="text" onChange={this.handleChange} 
+        placeholder="enter an amount" />
+        <input type="button" onClick={this.deposit} 
+        value="Deposit" />
         <input type="button" value="Withdraw" />
       </div>
     )
